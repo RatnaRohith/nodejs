5a8b0b6925d71c43edfc79fa4c45fe71257cf553
Rohith <rohith.boddukuri@gmail.com>
2020-11-21 12:06:45 +0530

    first commit

---
+++ path.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+//Base name
+console.log(path.basename(__filename));
+
+//dir name
+console.log(path.dirname(__filename));
+
+//File Extension
+console.log(path.extname(__filename));
+
